6519146b19b811edeb8e99cb6521e7baa77c164e
Lucas Avila <LucasAvila08@outlook.com>
2025-07-24 01:41:15 -0300

    Add: Assets

--- DialogueSystemProject/Assets/Scripts/Controllers/InputsController.cs
+++ DialogueSystemProject/Assets/Scripts/Controllers/InputsController.cs
@@ -3,14 +3,14 @@ using TMPro;
 
 public class InputsController : MonoBehaviour
 {
-    [SerializeField] private DialogueManager _dialogueManager;
     [SerializeField] private DialogueController _dialogueController;
+    [SerializeField] string _dialogueKey;
     [SerializeField] private SimpleDialogueController _simpleDialogueController;
     [SerializeField] private TMP_Dropdown _languageDropdown;
 
-    public void StartDialogue(string key)
+    public void StartDialogue()
     {
-        _dialogueController.StartDialogue(key);
+        _dialogueController.StartDialogue(_dialogueKey);
     }
 
     public void ConsumeDialogue()
